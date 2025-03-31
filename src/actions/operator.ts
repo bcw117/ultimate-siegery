b1b8f079eeac16467a90cf9e6c7c11377e08f78c
Boris Wang <boris.wang117@gmail.com>
2025-03-30 21:44:22 -0700

    Lint

--- src/actions/operator.ts
+++ src/actions/operator.ts
@@ -7,11 +7,7 @@
 import { Operator, OperatorWithLoadout } from "@/lib/types/operator";
 import { Weapon } from "@/lib/types/weapon";
 import { Gadget } from "@/lib/types/gadget";
-import {
-  getRandomElement,
-  getRandomNumber,
-  OPERATOR_COUNT,
-} from "@/utils/helpers";
+import { getRandomElement } from "@/utils/helpers";
 import { createClient } from "@/utils/supabase/server";
 
 /**
