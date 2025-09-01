9ec399a26155cfc73c2f0d98880cd1842358a98b
Boris Wang <boris.wang117@gmail.com>
2025-09-01 15:43:26 -0700

    Revert config to use environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,24 @@
 import createMDX from "@next/mdx";
+
+const s3_url = process.env.NEXT_PUBLIC_SUPABASE_URL?.replace("https://", "");
+
+if (!s3_url) {
+  throw Error("NEXT_PUBLIC_SUPABASE_URL not set");
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Image configuration for Supabase storage
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        // This is your DEV hostname
-        hostname: "hhcizyfqkhsyspojddcc.supabase.co",
+        hostname: s3_url,
         port: "",
-        pathname: "/storage/v1/object/public/**",
+        pathname: "/**",
       },
-      {
-        protocol: "https",
-        // This is your PROD hostname
-        hostname: "lbowlaynaxcptunzgtog.supabase.co",
-        port: "",
-        pathname: "/storage/v1/object/public/**",
-      },
+      {},
     ],
   },
 };
