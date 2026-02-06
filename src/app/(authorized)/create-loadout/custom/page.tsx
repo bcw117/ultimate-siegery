26b7fe88b880f190b8f4b3ec2905fd386835a687
Boris Wang <boris.wang117@gmail.com>
2026-02-06 00:58:15 -0800

    fix build error

--- src/app/(authorized)/create-loadout/custom/page.tsx
+++ src/app/(authorized)/create-loadout/custom/page.tsx
@@ -2,8 +2,11 @@ import CustomLoadoutView from "@/components/view/CustomLoadoutView";
 import { fetchAllOperatorLoadouts } from "@/lib/api/operators/queries";
 import { isNil } from "lodash";
 
+export const dynamic = "force-dynamic";
+
 export default async function CustomLoadout() {
   const response = await fetchAllOperatorLoadouts();
+
 
   if (!response.ok || isNil(response.data)) {
     throw new Error("Failed to fetch operator data");
--- src/app/(authorized)/create-loadout/random/page.tsx
+++ src/app/(authorized)/create-loadout/random/page.tsx
@@ -2,8 +2,11 @@ import RandomLoadoutView from "@/components/view/RandomLoadoutView";
 import { fetchAllOperatorLoadouts } from "@/lib/api/operators/queries";
 import { isNil } from "lodash";
 
+export const dynamic = "force-dynamic";
+
 export default async function RandomLoadout() {
   const response = await fetchAllOperatorLoadouts();
+
 
   if (!response.ok || isNil(response.data)) {
     throw new Error("Failed to fetch operator data");
