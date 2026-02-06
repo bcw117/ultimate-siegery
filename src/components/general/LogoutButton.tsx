dda325c31ccdfc5880c1e563a7b1e5a7b5affa3a
Boris Wang <boris.wang117@gmail.com>
2026-02-06 01:39:20 -0800

    Goofy ah logout states

--- src/components/general/LogoutButton.tsx
+++ src/components/general/LogoutButton.tsx
@@ -1,27 +1,13 @@
 "use client";
 
-import { Loader2 } from "lucide-react";
 import { Button } from "../ui/button";
-import { useState } from "react";
 import { SignOutButton } from "@clerk/nextjs";
 
 function LogOutButton() {
-  const [loading, setLoading] = useState(false);
-
-  const handleLogOut = async () => {
-    setLoading(true);
-    setLoading(false);
-  };
-
   return (
     <SignOutButton>
-      <Button
-        variant="outline"
-        onClick={handleLogOut}
-        disabled={loading}
-        className="w-24 cursor-pointer"
-      >
-        {loading ? <Loader2 className="animate-spin" /> : "Sign Out"}
+      <Button variant="outline" className="w-24 cursor-pointer">
+        {"Sign Out"}
       </Button>
     </SignOutButton>
   );
